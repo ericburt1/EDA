bd4287ec6cb8808ed2ae4021469c698c22e9e834
ericburt <ericburt@mail.weber.edu>
2023-02-03 12:22:52 -0700

    roomsNotDone

--- game.py
+++ game.py
@@ -1,1 +1,16 @@
-import random
+# 0's are where player gets no movement. 3 is the player. 1 is moveable spots
+rooms = [0,0,0,0,0,0,0,0,0,0,0,
+		 0,3,0,0,0,0,0,0,0,0,0,
+		 0,1,0,0,0,0,0,0,0,0,0,
+		 0,1,1,1,1,1,1,1,1,0,0,
+		 0,1,1,1,1,1,1,1,1,0,0,
+		 0,1,1,1,1,1,1,1,1,0,0,
+		 0,1,1,1,1,1,1,1,1,0,0,
+		 0,0,0,0,0,0,0,0,0,0,0]
+playerInput = int(input(f"""
+	Welcome to EDA.
+
+	What do you want to do?
+	1) Start Game
+	q) Quit
+		\n"""))
