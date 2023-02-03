1122bc147b1a37d370bbe3b913372d2836feb68c
ericburt <ericburt@mail.weber.edu>
2023-02-03 12:23:58 -0700

    welcome

--- game.py
+++ game.py
@@ -1,12 +1,4 @@
 # 0's are where player gets no movement. 3 is the player. 1 is moveable spots
-rooms = [0,0,0,0,0,0,0,0,0,0,0,
-		 0,3,0,0,0,0,0,0,0,0,0,
-		 0,1,0,0,0,0,0,0,0,0,0,
-		 0,1,1,1,1,1,1,1,1,0,0,
-		 0,1,1,1,1,1,1,1,1,0,0,
-		 0,1,1,1,1,1,1,1,1,0,0,
-		 0,1,1,1,1,1,1,1,1,0,0,
-		 0,0,0,0,0,0,0,0,0,0,0]
 playerInput = int(input(f"""
 	Welcome to EDA.
 
